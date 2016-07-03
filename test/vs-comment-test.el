cd33c38d02ebe92353b4f1a849d06d1837d09fe9
Gustaf Waldemarson <gustaf.waldemarson@gmail.com>
2016-07-03 23:20:34 +0200

    Added initial ert-runner files.

---
+++ test/vs-comment-test.el
@@ -0,0 +1,31 @@
+;;; vs-comment-test.el --- Face changing unit tests -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2016 Gustaf Waldemarson
+
+;; Author: Gustaf Waldemarson <gustaf.waldemarson@gmail.com>
+;; Maintainer: Gustaf Waldemarson <gustaf.waldemarson@gmail.com>
+
+;; This file is NOT part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;; Unittest code for `vs-comment-mode'.
+
+;;; Code:
+
+
+(provide 'vs-comment-test)
+
+;;; vs-comment-test.el ends here
